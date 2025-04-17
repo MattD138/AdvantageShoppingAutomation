d13ef037a9dc22ab77f000797ccca79b033ed1bb
Matt Donnelly <mail@mattdonnelly.com.au>
2025-04-17 23:33:41 +1000

    Uses Driver.Dispose in place of Driver.Quit

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -7,23 +7,17 @@ namespace AdvantageShoppingAutomation.Tests
     public class BaseTest
     {
         protected IWebDriver Driver;
+
         [OneTimeSetUp]
         public void GlobalSetup()
         {
             Driver = WebDriverFactory.CreateDriver("chrome");
         }
 
-        [SetUp]
-        public void NavigateToSite()
-        {
-            Driver.Navigate().GoToUrl(TestDataLoader.LoadBaseUrl());
-
-        }
-
         [OneTimeTearDown]
         public void GlobalTearDown()
         {
-            Driver.Quit();
+            Driver.Dispose();
         }
     }
 }
