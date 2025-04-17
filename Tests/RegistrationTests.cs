1f372bd6c06adef02761d5f419f47f13da9f3603
Matt Donnelly <mail@mattdonnelly.com.au>
2025-04-17 18:08:58 +1000

    Implements test case LOG001 in RegistrationTests

--- Tests/RegistrationTests.cs
+++ Tests/RegistrationTests.cs
@@ -1,4 +1,5 @@
 ﻿using AdvantageShoppingAutomation.Pages;
+using AdvantageShoppingAutomation.Utilities;
 
 namespace AdvantageShoppingAutomation.Tests
 {
@@ -7,7 +8,29 @@ namespace AdvantageShoppingAutomation.Tests
         [Test]
         public void LOG001_MandatoryFieldValidation_ShowsAndClearsErrors()
         {
+            UserData userData = TestDataLoader.LoadUserData();
 
+            // Navigate to /register via home page
+            var homePage = new HomePage(Driver);
+            homePage.ClickUserIcon();
+            homePage.ClickCreateNewAccountLink();
+
+            var registerPage = new RegistrationPage(Driver);
+            registerPage.TriggerMandatoryFieldErrors();
+
+            var registerFormErrors = registerPage.GetErrorMessages();
+
+            Assert.IsTrue(registerFormErrors.Any(e => e.Contains("Username field is required")));
+            Assert.IsTrue(registerFormErrors.Any(e => e.Contains("Email field is required")));
+            Assert.IsTrue(registerFormErrors.Any(e => e.Contains("Password field is required")));
+            Assert.IsTrue(registerFormErrors.Any(e => e.Contains("Confirm password field is required")));
+
+            registerPage.EnterMandatoryData(
+                userData.Username,
+                userData.Email,
+                userData.Password);
+
+            Assert.IsTrue(registerPage.AreErrorsCleared());
         }
     }
 }
