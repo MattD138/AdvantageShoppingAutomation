669ef13dbe195454aaad8c5cb203e4cb819662dd
Matt Donnelly <mail@mattdonnelly.com.au>
2025-04-17 15:49:43 +1000

    Adds helper class for visibility and interactivity waits

---
+++ Utilities/WaitHelper.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+
+namespace AdvantageShoppingAutomation.Utilities
+{
+    public static class WaitHelper
+    {
+        public static IWebElement WaitUntilVisible(IWebDriver driver, By locator, int timeoutInSeconds = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(ExpectedConditions.ElementIsVisible(locator));
+        }
+
+        public static IWebElement WaitUntilClickable(IWebDriver driver, By locator, int timeoutInSeconds = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(ExpectedConditions.ElementToBeClickable(locator));
+        }
+
+        public static bool WaitUntilInvisible(IWebDriver driver, By locator, int timeoutInSeconds = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeoutInSeconds));
+            return wait.Until(ExpectedConditions.InvisibilityOfElementLocated(locator));
+        }
+    }
+}
