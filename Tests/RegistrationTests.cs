67482cdbd70d02262bbc44583617a01f42b0e95b
Matt Donnelly <mail@mattdonnelly.com.au>
2025-04-17 14:47:32 +1000

    Removes unused imports

--- Tests/RegistrationTests.cs
+++ Tests/RegistrationTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AdvantageShoppingAutomation.Pages;
+﻿using AdvantageShoppingAutomation.Pages;
 
 namespace AdvantageShoppingAutomation.Tests
 {
