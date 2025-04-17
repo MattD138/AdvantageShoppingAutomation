1922cfd288e944d6ed6274f01d18ca2926079ea1
Matt Donnelly <mail@mattdonnelly.com.au>
2025-04-17 14:38:24 +1000

    Externalises base URL and adds a loader method

---
+++ Utilities/TestDataLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AdvantageShoppingAutomation.Utilities
+{
+    public class TestDataLoader
+    {
+        public static string LoadBaseUrl()
+        {
+            var json = File.ReadAllText("TestData/TestConfig.json");
+            var jObject = JObject.Parse(json);
+            return jObject["BaseUrl"].ToString();
+        }
+
+    }
+}
