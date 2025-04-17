5ff85cf5499e6116e52f27e80dfea32fbdfd7feb
Matt Donnelly <mail@mattdonnelly.com.au>
2025-04-17 18:06:57 +1000

    Implements selectors and methods in BasePage and RegistrationPage

--- Pages/BasePage.cs
+++ Pages/BasePage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using AdvantageShoppingAutomation.Utilities;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 
 namespace AdvantageShoppingAutomation.Pages
@@ -6,12 +7,38 @@ namespace AdvantageShoppingAutomation.Pages
     public class BasePage
     {
         protected readonly IWebDriver Driver;
-        protected WebDriverWait Wait;
         public BasePage(IWebDriver driver)
         {
             Driver = driver;
-            Wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
+
+        public void SafeClick(By locator)
+        {
+            var element = WaitHelper.WaitUntilClickable(Driver, locator);
+            element.Click();
+        }
+
+        public void SafeSendKeys(By locator, string value)
+        {
+            var element = WaitHelper.WaitUntilVisible(Driver, locator);
+            element.Clear();
+            element.SendKeys(value);
+        }
+
+        public bool ElementExists(By locator)
+        {
+            try { return Driver.FindElement(locator).Displayed; }
+            catch (NoSuchElementException) { return false; }
+        }
+
+        // Icon buttons in the website header, common to all pages
+        protected By UserIcon = By.Id("menuUser");
+        public void ClickUserIcon() => SafeClick(UserIcon);
+
+        // Login modal
+        protected By CreateNewAccountLink = By.ClassName("create-new-account");
+        public void ClickCreateNewAccountLink() => SafeClick(CreateNewAccountLink);
+
 
         public void Log(String message)
         {
--- Pages/RegistrationPage.cs
+++ Pages/RegistrationPage.cs
@@ -8,5 +8,37 @@ namespace AdvantageShoppingAutomation.Pages
         {
             // RegistrationPage Constructor
         }
+        // Registration form fields
+        private By UsernameFieldLocator = By.Name("usernameRegisterPage");
+        private By EmailFieldLocator = By.Name("emailRegisterPage");
+        private By PasswordFieldLocator = By.Name("passwordRegisterPage");
+        private By ConfirmPasswordFieldLocator = By.Name("confirm_passwordRegisterPage");
+        // Static element outside fields used to defocus
+        private By CreateAccountHeadingLocator = By.XPath("//div[@id='registerCover']//h3[text()='ACCOUNT DETAILS']");
+
+        public void TriggerMandatoryFieldErrors()
+        {
+            SafeClick(UsernameFieldLocator);
+            SafeClick(EmailFieldLocator);
+            SafeClick(PasswordFieldLocator);
+            SafeClick(ConfirmPasswordFieldLocator);
+            // Click outside the fields to trigger validation
+            SafeClick(CreateAccountHeadingLocator);
+        }
+
+        public void EnterMandatoryData(string username, string email, string password)
+        {
+            SafeSendKeys(UsernameFieldLocator, username);
+            SafeSendKeys(EmailFieldLocator, email);
+            SafeSendKeys(PasswordFieldLocator, password);
+            SafeSendKeys(ConfirmPasswordFieldLocator, password);
+        }
+
+        private By registerFormErrorLabelLocator = By.XPath("//div[@id='registerCover']//label[@class='invalid']");
+        private List<IWebElement> ErrorMessages => Driver.FindElements(registerFormErrorLabelLocator).ToList();
+        public List<string> GetErrorMessages() => ErrorMessages.Select(e => e.Text).ToList();
+
+        public bool AreErrorsCleared() => !ErrorMessages.Any();
+
     }
 }
